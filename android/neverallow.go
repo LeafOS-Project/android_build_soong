11b9eca3753715ed0d735a4b73dacf75a3fee9c3
Tim Zimmermann <tim@linux4.de>
2025-01-11 17:22:04 +0100

    Add vendor/leaf to kotlin plugin whitelist
    Change-Id: If5013a9a39dd1342095760cf70050c9de6375b3e

--- android/neverallow.go
+++ android/neverallow.go
@@ -291,6 +291,7 @@ func createKotlinPluginRule() []Rule {
 		// Actual path prebuilts/sdk/current/androidx/m2repository/androidx/compose/compiler/compiler-hosted
 		"prebuilts/sdk/current/androidx",
 		"external/kotlinc",
+		"vendor/leaf/kotlin",
 	}
 
 	return []Rule{
